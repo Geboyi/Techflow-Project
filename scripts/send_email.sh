f21e3ce40e2dc9a12e999c8783dc9c936ffd7587
Geboyi <106437942+Geboyi@users.noreply.github.com>
2026-03-09 22:23:49 +0000

    email deployment

--- scripts/send_email.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-STATUS=$1
-TO="dlaregboyi@gmail.com"
-SUBJECT="TechFlow Deployment Status: $STATUS"
-BODY="Deployment finished with status: $STATUS"
-
-echo "$BODY" | mail -s "$SUBJECT" "$TO"
